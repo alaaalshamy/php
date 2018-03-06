c0b3a47ceab701c2fd16e5b8d06f16bc53f2e77e
Losh_lo <alaaalshamy44@yahoo.com>
2018-03-06 02:35:17 +0200

    Revert "start the project !!"
    This reverts commit c8f9135

--- DjangoProject/liberary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LiberaryConfig(AppConfig):
-    name = 'liberary'
--- DjangoProject/liberary/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Writer(models.Model):
-    writer_name = models.CharField(max_length=200)
-    dateOfBirth = models.DateTimeField('date published')
-
